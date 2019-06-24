6129b55559abb902711c1aa779454f0758435993
fmpaci <fmpaci88@gmail.com>
2019-06-23 21:33:39 -0300

    Actualizado con AgendaCitas: 	@agregarNuevoDia 	@abogados

---
+++ TPO_PII1C2019/src/algoritmos/TestVariosFPA.java
@@ -0,0 +1,36 @@
+package algoritmos;
+
+import tdas.ConjuntoTDA;
+
+public class TestVariosFPA {
+
+	public static void main(String[] args) {
+		implementaciones.ImpAgendaCitas citas = new implementaciones.ImpAgendaCitas();
+		ConjuntoTDA abogados = new implementaciones.ImpConjunto();
+		String aux = "lol";
+		//aux = null;
+		abogados.inicializar();
+		citas.inicializar();
+		
+		citas.agregarNuevoDia("Alberto", "hoy", "23/06/2019");
+		citas.agregarNuevoDia("Alberto", "mañana", "24/06/2019");
+		citas.agregarNuevoDia("Juan", "hoy", "23/06/2019");
+		citas.agregarNuevoDia("Juan", "mañana", "24/06/2019");
+		citas.agregarNuevoDia("pedro", "hoy", "23/06/2019");
+		citas.agregarNuevoDia("Ramon", "hoy", "23/06/2019");
+		citas.agregarNuevoDia("Jose", "hoy", "23/06/2019");
+		citas.agregarNuevoDia("Matias", "hoy", "23/06/2019");
+		
+		
+		abogados = citas.abogados();
+		
+		while(!abogados.conjuntoVacio()){
+			aux = abogados.elegir();
+			System.out.println(aux);
+			abogados.sacar(aux);
+		}
+		System.out.println("Terminado");
+	}
+	
+
+}
--- TPO_PII1C2019/src/implementaciones/ImpAgendaCitas.java
+++ TPO_PII1C2019/src/implementaciones/ImpAgendaCitas.java
@@ -11,41 +11,68 @@ public class ImpAgendaCitas implements AgendaCitasTDA {
 	
 	@Override
 	public void inicializar() {
-		primerAgenda.abogado = null;
-		primerAgenda.primeraFecha = null;
-		primerAgenda.sigAbogado = null;
+		primerAgenda = null;
 
 	}
+//funciones auxiliares para agregar nuevo día
+	private NodoAgenda crearAbogado(String abogado) {
+		NodoAgenda auxAgenda = new NodoAgenda();
+		
+		auxAgenda.abogado = abogado;
+		auxAgenda.primeraFecha = null;
+		auxAgenda.sigAbogado = null;
+		
+		return auxAgenda;
+	}
+	
+	private NodoDia crearDia(String dia, String fecha) {
+		NodoDia auxDia = new NodoDia();
 
+		auxDia.dia = dia;
+		auxDia.fecha = fecha;
+		//Descomentar!!!!
+		//auxDia.turnos = new ImpArbolCitas();
+		//auxDia.turnos.inicializar();
+		auxDia.siguienteFecha = null;
+		
+		return auxDia;
+	}
+//funciones auxiliares para agregar nuevo día	
+	
+	private boolean mismaFecha(String dia, String fecha, NodoDia ndia) {
+		return dia.equalsIgnoreCase(ndia.dia) && fecha.equalsIgnoreCase(ndia.fecha);
+	}
+	
+	
 	@Override
 	public void agregarNuevoDia(String abogado, String dia, String fecha) {
-		//Si no hay ningun abogado, creo el primero y su correspondiente
-		//fecha y día
-		if(primerAgenda.abogado == null) {
-			primerAgenda.abogado = abogado;
-	
-			NodoDia ndia = new NodoDia();
-			
-			ndia.dia = dia;
-			ndia.fecha = fecha;
-			//ndia.turnos = new ImpArbolCitas();
-			//ndia.turnos.inicializar();
-			ndia.siguienteFecha = null;
-			
-			primerAgenda.primeraFecha = ndia;
+		NodoAgenda aux = new NodoAgenda();
+		aux = primerAgenda;
+		if(aux == null) {
+			primerAgenda = crearAbogado(abogado);
+			primerAgenda.primeraFecha = crearDia(dia,fecha);
 		}else {
-			NodoAgenda aux = new NodoAgenda();
-			aux = primerAgenda;
-			while(primerAgenda.sigAbogado != null && !aux.abogado.equalsIgnoreCase(abogado) ) {
+			//busco si existe el abogado o me quedo con el último
+			while(aux.sigAbogado != null && !aux.abogado.equalsIgnoreCase(abogado) ) {
 				aux = aux.sigAbogado;
 			}
-			
-		
+			//si lo encontró, solo creo el día
+			if(aux.abogado.equalsIgnoreCase(abogado)) {
+				NodoDia auxDia = new NodoDia();
+				auxDia = aux.primeraFecha;
+				while(auxDia.siguienteFecha != null && !mismaFecha(dia,fecha,auxDia)) {
+					auxDia = auxDia.siguienteFecha;
+				}
+				//si no encuentra la fecha, la crea
+				if(auxDia.siguienteFecha == null) auxDia.siguienteFecha = crearDia(dia,fecha);
+			}else {
+				aux.sigAbogado = crearAbogado(abogado);
+				aux.sigAbogado.primeraFecha = crearDia(dia,fecha);
+			}
 		}
-		
-
 	}
 
+	
 	@Override
 	public void agregarNuevaCita(String abogado, String fecha, String hora, String cliente) {
 		// TODO Auto-generated method stub
@@ -84,8 +111,15 @@ public class ImpAgendaCitas implements AgendaCitasTDA {
 
 	@Override
 	public ConjuntoTDA abogados() {
-		// TODO Auto-generated method stub
-		return null;
+		NodoAgenda aux = new NodoAgenda();
+		ConjuntoTDA conjAbogados = new ImpConjunto();
+		conjAbogados.inicializar();
+		aux = primerAgenda;
+		while(aux != null) {
+			conjAbogados.agregar(aux.abogado);
+			aux = aux.sigAbogado;
+		}
+		return conjAbogados;
 	}
 
 	@Override
