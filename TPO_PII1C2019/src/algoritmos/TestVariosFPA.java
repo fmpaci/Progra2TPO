4f8c64e7d9beac6a9ab438e145525531c938abe5
fmpaci <fmpaci88@gmail.com>
2019-07-02 01:21:32 -0300

    Commit Final 2

--- TPO_PII1C2019/src/algoritmos/TestVariosFPA.java
+++ TPO_PII1C2019/src/algoritmos/TestVariosFPA.java
@@ -1,59 +1,56 @@
 package algoritmos;
 
-import org.junit.Assert;
 
 import implementaciones.AgendaCitas;
-import implementaciones.ColaPrioridad;
-import tdas.ConjuntoTDA;
+
 import tdas.ColaPrioridadTDA;
 
 public class TestVariosFPA {
 
 	public static void main(String[] args) {
-		implementaciones.AgendaCitas citas = new implementaciones.AgendaCitas();
-		IAlgoritmo algo = new Algoritmo(); 
-		citas.inicializar();
-		
+		System.out.println("iniciando");
 		Algoritmo algoritmo = new Algoritmo();
 		AgendaCitas agenda = new AgendaCitas();
 		agenda.inicializar();
 		
-		
+		agenda.agregarNuevoDia("un abogado", "lunes", "2019/01/07");
+		agenda.agregarNuevoDia("un abogado", "martes", "2019/01/08");
+		agenda.agregarNuevoDia("un abogado", "miercoles", "2019/01/09");
+		agenda.agregarNuevoDia("un abogado", "jueves", "2019/01/10");
 		agenda.agregarNuevoDia("un abogado", "martes", "2019/01/01");
-		agenda.agregarNuevaCita("un abogado", "2019/01/01", "09:00", "un cliente");
-		agenda.agregarNuevaCita("un abogado", "2019/01/01", "09:30", "un cliente");
-		agenda.agregarNuevoDia("un abogado", "lunes", "2018/12/31");
-		agenda.agregarNuevaCita("un abogado", "2018/12/31", "11:30", "un cliente");
-		agenda.agregarNuevaCita("un abogado", "2018/12/31", "10:30", "otro cliente");
-		String[] primeraReunionEsperada = new String[] { "un abogado", "2018/12/31", "11:30" };
-		String[] segundaReunionEsperada = new String[] { "un abogado", "2019/01/01", "09:00" };
-		String[] terceraReunionEsperada = new String[] { "un abogado", "2019/01/01", "09:30" };
 		
-		// Operación
-		String[][] reuniones = algoritmo.conQuienSeReunio(agenda, "un cliente");
+		agenda.agregarNuevaCita("un abogado", "2019/01/01", "00:00", "un cliente");
+		
+		
+		agenda.agregarNuevoDia("otro abogado", "martes", "2019/01/01");
+		agenda.agregarNuevaCita("otro abogado", "2019/01/01", "23:30", "un cliente");
 
-		System.out.println(reuniones.length);
-		
-		System.out.println(reuniones[0][0]);
-		System.out.println(reuniones[0][1]);
-		System.out.println(reuniones[0][2]);
-		
-		System.out.println(reuniones[1][0]);
-		System.out.println(reuniones[1][1]);
-		System.out.println(reuniones[1][2]);
-		
-		System.out.println(reuniones[2][0]);
-		System.out.println(reuniones[2][1]);
-		System.out.println(reuniones[2][2]);
-	
-		ColaPrioridadTDA horariosFree = new ColaPrioridad();
-		horariosFree.inicializar();
-		horariosFree = algoritmo.libresTotal(agenda, "2018/12/31");
-		while(!horariosFree.colaVacia()) {
-			System.out.println(horariosFree.primero());
-			System.out.println(horariosFree.prioridad());
-			horariosFree.desacolar();
+		// Operación
+		ColaPrioridadTDA horariosLibres = algoritmo.libresTotal(agenda, "2018/12/31");
+		int cantidadElementosCola = 0;
+		String primerAbogado = horariosLibres.primero();
+		String ultimoAbogado = "";
+		int i = 1;
+		while (!horariosLibres.colaVacia()) {
+			ultimoAbogado = horariosLibres.primero();
+			System.out.println(i +horariosLibres.primero() +" " + horariosLibres.prioridad());
+			i++;
+			horariosLibres.desacolar();
+			cantidadElementosCola++;
 		}
+
+		// Validación
+
+		/**
+		 * 48 turnos por día 2 abogados 2 turnos no disponibles (48*2)-2 = 94
+		 */
+		
+		
+		
+		
+		System.out.println(cantidadElementosCola);
+		System.out.println(primerAbogado);//"otro abogado
+		System.out.println(ultimoAbogado);//"un abogado"
 	}
 	
 
